e5a4d57059689b28f6d9f96e09aa181d057c1e77
Smash_Bash <williambashford@mac.com>
2024-10-02 12:54:51 +0100

    Initial Script Additions
    + Added PlayerScript, Weapon, GenericMagic, and DemoWeapon scripts.
    + Added the Weapon Base prefab, as well as a 'Fire Magic' variant.

---
+++ Assets/Scripts/DemoGun.cs
@@ -0,0 +1,28 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class DemoGun : Weapon
+{
+    public ParticleSystem bullets;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+
+    }
+    public override void HeldUpdate(PlayerScript player)
+    {
+        base.HeldUpdate(player);
+        if (Input.GetMouseButtonDown(0))
+        {
+            bullets.Emit(1);
+        }
+    }
+}
