f2b098e80dec6471ec2f39e34e64e130e2da6ff0
jaa3431 <jaa3431@my.acm.ac.uk>
2024-10-12 00:17:40 +0100

    Able to Read Primary & Secondary Abilities
    This commit, I managed to ensure that whenever the player presses either 1 or 2 (Primary/Secondary button) a public function would be able to call to able to read both primary and secondary abilities.
    
    This tasks is not 100% complete from my end as there may need to be more instructions inside these functions.

--- Assets/Scripts/playerControls/playerInput.cs
+++ Assets/Scripts/playerControls/playerInput.cs
@@ -8,6 +8,8 @@ public class playerInput : MonoBehaviour
     private PlayerControls controls;
     private InputAction playerMovement;
     private InputAction playerLook;
+    private InputAction playerPrimary; //This is the Variable for the Primary Ability
+    private InputAction playerSecondary; //This is the Variable for the Second Ability
    
     private Vector2 movementInput;
     private Vector2 cameraInput;
@@ -18,9 +20,17 @@ public class playerInput : MonoBehaviour
 
         playerMovement = controls.Player.Move;
         playerLook = controls.Player.Camera;
+        playerPrimary = controls.Player.primaryAbility;
+        playerSecondary = controls.Player.secondaryAbility;
 
+        //Whenever the player presses either primary or secondary ability, this calls a function to happen
+        playerPrimary.performed += PrimaryAbility;
+        playerSecondary.performed += SecondaryAbility;
+        
         playerMovement.Enable();
         playerLook.Enable();
+        playerPrimary.Enable();
+        playerSecondary.Enable();
     }
 
     public Vector2 getCameraInput()
@@ -35,9 +45,22 @@ public class playerInput : MonoBehaviour
         return movementInput;   
     }
 
+    //This functions only calls when one of the abilities are performed from the input action asset
+    public void PrimaryAbility(InputAction.CallbackContext primary)
+    {
+        Debug.Log("Bang!");
+    }
+
+    public void SecondaryAbility(InputAction.CallbackContext secondary)
+    {
+        Debug.Log("Boom");
+    }
+
     private void OnDisabled()
     {
         playerMovement.Disable();
         playerLook.Disable();
+        playerPrimary.Disable();
+        playerSecondary.Disable();
     }
 }
