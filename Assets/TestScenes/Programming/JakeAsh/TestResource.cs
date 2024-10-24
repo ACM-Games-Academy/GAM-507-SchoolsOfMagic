2e2d40b13e5e023a36e1cdb85f688e04fa380177
jaa3431 <jaa3431@my.acm.ac.uk>
2024-10-24 15:00:45 +0100

    Minimum & Maximum Resources
    This commit is testing to see if the resources go below 0 or above the max resource, if either those conditions occur then this is an issue.
    
    Luckily from my testing, the resources did not go below the minimum or above the maximum.

--- Assets/TestScenes/Programming/JakeAsh/TestResource.cs
+++ Assets/TestScenes/Programming/JakeAsh/TestResource.cs
@@ -19,6 +19,36 @@ public class TestResource : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        
+        if (Input.GetKeyDown(KeyCode.Alpha1)) 
+        {
+            playerModel.addBlood(20f);
+            Debug.Log("blood increase. blood = " + playerModel.getBlood());
+
+        }
+
+        if (Input.GetKeyDown(KeyCode.Alpha2))
+        {
+            playerModel.reduceBlood(20f);
+            Debug.Log("blood decrease. blood = " + playerModel.getBlood());
+
+        }
+
+        if (Input.GetKeyDown(KeyCode.Alpha3))
+        {
+            playerModel.addIron(5f);
+            Debug.Log("iron increase. iron = " + playerModel.getIron());
+
+        }
+
+        if (Input.GetKeyDown(KeyCode.Alpha4))
+        {
+            playerModel.reduceIron(5f);
+            Debug.Log("iron decrease. iron = " + playerModel.getIron());
+        }
+
     }
+
+
+
+
 }
