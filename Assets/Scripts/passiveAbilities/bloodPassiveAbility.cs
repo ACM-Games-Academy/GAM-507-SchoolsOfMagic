1d5d63ca03d63775ee0ea842e177fe2d156e06b4
jaa3431 <jaa3431@my.acm.ac.uk>
2024-10-27 20:30:49 +0000

    Chaning Passive Ability Classes
    In this commit I have created 4 passive abiliy classes that inherit the Player Base.
    
    In the Passive Controller I implement a method of a certain magic passive ability that would be the current player base script.

--- Assets/Scripts/passiveAbilities/bloodPassiveAbility.cs
+++ Assets/Scripts/passiveAbilities/bloodPassiveAbility.cs
@@ -2,10 +2,10 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
-public class passiveAbility : passiveBase
+public class bloodPassiveAbility : passiveBase
 {
     public override void updatePassive()
     {
-        
+
     }
 }
--- Assets/Scripts/passiveAbilities/passiveController.cs
+++ Assets/Scripts/passiveAbilities/passiveController.cs
@@ -7,6 +7,12 @@ public class passiveController : MonoBehaviour
 {
     [SerializeField] playerInput playerInput;
 
+    private passiveBase currentPassive;
+    [SerializeField] passiveBase naturePassive;
+    [SerializeField] passiveBase bloodPassive;
+    [SerializeField] passiveBase metalPassive;
+    [SerializeField] passiveBase arcanePassive;
+
     private void OnEnable()
     {
         playerInput.NatureMagic += changeMagicNature;
@@ -17,22 +23,22 @@ public class passiveController : MonoBehaviour
 
     private void changeMagicNature(object sender, EventArgs e)
     {
-
+        currentPassive = naturePassive;
     }
 
     private void changeMagicBlood(object sender, EventArgs e)
     {
-
+        currentPassive = bloodPassive;
     }
 
     private void changeMagicMetal(object sender, EventArgs e)
     {
-
+        currentPassive = metalPassive;
     }
 
     private void changeMagicArcane(object sender, EventArgs e)
     {
-
+        currentPassive = arcanePassive;
     }
 
     private void OnDisable()
