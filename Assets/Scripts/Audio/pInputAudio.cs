cae90faa416b1b04b3eb3e3d3ceea1e4c1780569
Launcelot Raven <launcelotraven@gmail.com>
2024-11-03 20:42:21 +0000

    pinput changes

--- Assets/Scripts/Audio/pInputAudio.cs
+++ Assets/Scripts/Audio/pInputAudio.cs
@@ -15,7 +15,7 @@ public class playerInputAudio : MonoBehaviour
     public AK.Wwise.State metalState;
     public AK.Wwise.State arcaneState;
 
-    private GameObject playerStats;
+    private playerModel playerStats;
     public CharacterController controller;
     private AK.Wwise.RTPC health = null;
     private float speed = 0f;
@@ -24,12 +24,12 @@ public class playerInputAudio : MonoBehaviour
     
     private void Awake()
     {
-        playerStats = GameObject.GetComponent<playerModel>;
+        playerStats = GetComponent<playerModel>();
     }
     
     private void Update()
     {
-        health.SetValue(player, playerStats.getHealth);
+        health.SetValue(player, playerStats.getHealth());
         timer = Mathf.Max(timer, timer - Time.deltaTime);
     }
 
@@ -38,8 +38,8 @@ public class playerInputAudio : MonoBehaviour
         speed = (transform.position - lastPos).magnitude;
         if (speed > 0f && timer <= 0 && controller.isGrounded)
         {
-            footsteps.Post();
-            timer = 0.31415926535;
+            footsteps.Post(this.gameObject);
+            timer = 0.31415926535f;
         }
 
         lastPos = transform.position;
