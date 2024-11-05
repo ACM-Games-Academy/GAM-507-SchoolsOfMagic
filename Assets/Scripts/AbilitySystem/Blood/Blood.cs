529621ac783e06681e3605db1487e7e6fd3557fb
Launcelot Raven <launcelotraven@gmail.com>
2024-11-05 15:23:46 +0000

    lots of changes
    moved the new ability system to the scripts folder
    made magic base inherited scripts for each magic class

---
+++ Assets/Scripts/AbilitySystem/Blood/Blood.cs
@@ -0,0 +1,43 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+namespace Magic
+{
+    public class Blood : MagicBase
+    {
+        public const MagicType magicType = MagicType.Blood;
+        private MagicController magicController;
+        //private GameObject ironGripPrefab;
+
+        //Constructs a new Metal object
+        public Blood(MagicController _magicController)
+        {
+            magicController = _magicController;
+            //_magicController.TryGetPrefab("IronGrip", out ironGripPrefab);
+        }
+
+        // Called when Metal is equipped in MagicController object
+        public override void Equip()
+        {
+
+        }
+
+        //Called when Metal is unequipped in a MagicController object
+        public override void Unequip()
+        {
+
+        }
+
+        //Called when Metal is updated in a MagicController object
+        public override void Update()
+        {
+            //TODO: Write in input controls for Metal abilities
+            //Example of how an input control and subsequent prefab instantiation may look
+            //if (Input.GetKeyDown(KeyCode.W))
+            //{
+            //    GameObject grip = Object.Instantiate(ironGripPrefab);
+            //}
+        }
+    }
+}
