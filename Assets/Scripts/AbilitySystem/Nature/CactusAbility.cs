0d587548a4f1b68e0052b0de0e6c4da5cc37512f
Smash_Bash <williambashford@mac.com>
2024-11-07 16:31:52 +0000

    Cactus Spell Prefab
    + Added the prefab for the Cactus Spell.

--- Assets/Scripts/AbilitySystem/Nature/CactusAbility.cs
+++ Assets/Scripts/AbilitySystem/Nature/CactusAbility.cs
@@ -2,20 +2,28 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
-public class NatureAbility : MonoBehaviour
+public class CactusAbility : MonoBehaviour
 {
     public playerController controller;
+    public float lifetime = 1;
+    public AnimationCurve sizeCurve;
 
     // Start is called before the first frame update
     void Start()
     {
-        
+        DamageEnemies();
     }
 
     // Update is called once per frame
     void Update()
     {
-        DamageEnemies();
+        lifetime -= Time.deltaTime;
+        if (lifetime <= 0)
+        {
+            Destroy(gameObject);
+        }
+        float size = sizeCurve.Evaluate(1 - lifetime);
+        transform.localScale = new Vector3(size, size, size);
     }
 
     public void DamageEnemies()
@@ -26,7 +34,7 @@ public class NatureAbility : MonoBehaviour
         {
             if (Vector3.Distance(transform.position, enemy.transform.position) < 5)
             {
-                enemy.health -= 50;
+                enemy.Damage(50);
                 enemy.GetComponent<Rigidbody>().AddExplosionForce(1000, transform.position, 5);
             }
         }
