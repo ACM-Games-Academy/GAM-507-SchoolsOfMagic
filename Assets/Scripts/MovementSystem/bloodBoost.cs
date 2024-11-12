2b9163b7bd00c10a90e2a58843312ac9a44d0104
Jack Brettell <jwb3994@my.acm.ac.uk>
2024-11-12 22:16:01 +0000

    Started tank

--- Assets/Scripts/MovementSystem/bloodBoost.cs
+++ Assets/Scripts/MovementSystem/bloodBoost.cs
@@ -13,7 +13,7 @@ public class bloodBoost : movementAbility
         RaycastHit hit;
         if (player.controller.isGrounded && Physics.Raycast(transform.position + (Vector3.down * 0.95f), transform.TransformDirection(Vector3.down), out hit, 1, layersToIgnore))
         {
-            print(hit.transform.gameObject.name);
+           // print(hit.transform.gameObject.name);
             if (hit.transform.gameObject.name.Contains("Blood") || (Input.GetKey(KeyCode.LeftShift) && player.playerController.GetBlood() > 0))
             {
                 if (!hit.transform.gameObject.name.Contains("Blood"))
