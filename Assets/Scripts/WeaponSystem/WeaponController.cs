9047f24ea8f56d9874d47e3011f243fed2ae4a96
Launcelot Raven <launcelotraven@gmail.com>
2024-11-21 17:04:36 +0000

    Added weapon reloading
    weapon reloading now uses iron
    weapons can now all reload and won't fire if there isnt enouhg ammo
    won't reload if there isnt enough iron and will only reload however much iron is left

--- Assets/Scripts/WeaponSystem/WeaponController.cs
+++ Assets/Scripts/WeaponSystem/WeaponController.cs
@@ -132,11 +132,11 @@ public class WeaponController : MonoBehaviour
     {
         if (!isReloading)
         {
-            StartCoroutine(ReloadCoroutine());
+            ReloadCurrentGun();
         }
     }   
 
-    private IEnumerator ReloadCoroutine()
+    private void ReloadCurrentGun()
     {
         WeaponStats weaponStats = currentWeapon.WeaponStats;
         int reloadedAmount = 0;
@@ -146,14 +146,27 @@ public class WeaponController : MonoBehaviour
 
         int ammoDeficit = currentWeapon.WeaponStats.MagazineCapacity - currentWeapon.CurrentAmmo;
 
-        if (controller.GetIron() - ammoDeficit < 0)
+        if (controller.GetIron() <= 0)
+        {;
+            reloadedAmount = 0;
+        }
+        else if (controller.GetIron() - ammoDeficit >= 0f)
         {
             reloadedAmount = ammoDeficit;
+
+            StartCoroutine(ReloadCoroutine(reloadedAmount));
         }
         else
         {
             reloadedAmount = Convert.ToInt32(controller.GetIron());
+
+            StartCoroutine(ReloadCoroutine(reloadedAmount));
         }
+    }
+
+    private IEnumerator ReloadCoroutine(int reloadedAmount)
+    {
+        WeaponStats weaponStats = currentWeapon.WeaponStats;
 
         isReloading = true;
         yield return new WaitForSeconds(weaponStats.ReloadSpeed);
@@ -162,7 +175,7 @@ public class WeaponController : MonoBehaviour
         //if not the gun won't reload
         if (weaponStats == currentWeapon.WeaponStats)
         {
-            currentWeapon.AddAmmo(reloadedAmount);
+            currentWeapon.AddAmmo(Convert.ToInt32(reloadedAmount));
             controller.AddReduceValue(PlayerController.ValueType.Iron, -reloadedAmount, false);
         }
 
